8a354a149085d92264f3def4d7981344bf00b268
joel <jcchangg3@gmail.com>
2024-04-10 07:44:18 +0800

    Lint fixes
    Signed-off-by: joel <jcchangg3@gmail.com>

--- pom/objects/main_nav.py
+++ pom/objects/main_nav.py
@@ -1,17 +1,19 @@
-from time import sleep
-from pom.locators.Locators import MainNavLocators
-from pom.locators.Locators import CategoryDropdownOptions
-from selenium.webdriver.common.by import By
+'''pom for elements'''
+import sys
+from dataclasses import dataclass
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import sys
+from pom.locators.locators import MainNavLocators
+from pom.locators.locators import CategoryDropdownOptions
 sys.path.append(sys.path[0] + "/....")
 # import os
 # Uncomment if the above example gives you a relative path error
 # sys.path.append(os.getcwd())
 
 
-class Dropdown(object):
+@dataclass
+class Dropdown:
+    '''Object to represent dropdown element'''
     def __init__(self, driver, root):
         self.driver = driver
         self.root = root
@@ -26,7 +28,9 @@ class Dropdown(object):
         self.mp3 = root.find_element(*CategoryDropdownOptions.MP3)
 
 
-class MainNav(object):
+
+class MainNav:
+    '''Object main nav element '''
     def __init__(self, driver, root):
         self.driver = driver
         self.root = root
@@ -34,8 +38,10 @@ class MainNav(object):
         self.dropdown_toggle = root.find_element(
             *MainNavLocators.DROPDOWN_TOGGLE)
         self.search_button = root.find_element(*MainNavLocators.SEARCH_BUTTON)
+        self.dropdown_options = None
 
     def expand_dropdown(self):
+        '''encapsulate actions needed to expand the main nav's dropdown'''
         WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located(MainNavLocators.DROPDOWN_TOGGLE))
         self.dropdown_toggle.click()
