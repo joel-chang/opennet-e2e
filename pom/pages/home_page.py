8c1565baa0eba5104e675ee63dd46e6e8f3a5496
joel <jcchangg3@gmail.com>
2024-04-10 07:46:43 +0800

    Lint fixes
    Signed-off-by: joel <jcchangg3@gmail.com>

--- pom/pages/home_page.py
+++ pom/pages/home_page.py
@@ -1,15 +1,16 @@
-from pom.locators.Locators import HomePageLocators as Locators
-from selenium.webdriver.common.by import By
+'''To represent HomePage'''
 import sys
+from pom.locators.locators import HomePageLocators as Locators
 
-from pom.objects.MainNav import MainNav
+from pom.objects.main_nav import MainNav
 sys.path.append(sys.path[0] + "/....")
 # import os
 # Uncomment if the above example gives you a relative path error
 # sys.path.append(os.getcwd())
 
 
-class HomeP(object):
+class HomeP:
+    '''To represent home page'''
     def __init__(self, driver):
         self.driver = driver
         self.main_nav = MainNav(
